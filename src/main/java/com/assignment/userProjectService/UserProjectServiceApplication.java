6c262ced8cfb832a07b33b83b838136514e63bc6
Yaswanth <yashreddy786@gmail.com>
2020-07-29 16:40:12 +0530

    added logger to the application

--- src/main/java/com/assignment/userProjectService/UserProjectServiceApplication.java
+++ src/main/java/com/assignment/userProjectService/UserProjectServiceApplication.java
@@ -1,15 +1,19 @@
 package com.assignment.userProjectService;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class UserProjectServiceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UserProjectServiceApplication.class, args);
+	
 	}
+	
+	
 
 }
--- src/main/java/com/assignment/userProjectService/controller/GtsUserProjectController.java
+++ src/main/java/com/assignment/userProjectService/controller/GtsUserProjectController.java
@@ -2,6 +2,9 @@ package com.assignment.userProjectService.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,22 +24,26 @@ import com.assignment.userProjectService.repository.GTS_USER_PROJECT_Repository;
 public class GtsUserProjectController<T> {
 	@Autowired
 	private GTS_USER_PROJECT_Repository gtsUserProjectRepository;
-	
-	List<GtsUserProjects> workExperiences = new ArrayList();
+	private static final Logger logger = LoggerFactory.getLogger(GtsUserProjectController.class);
+	List<GtsUserProjects> projects = new ArrayList();
 	@GetMapping("api/v1/user/project/user_id")
 	public <T> T getUserProjectsByUserId(@RequestBody UserId userId) {
 
 		Long GTS_USER_ID=userId.getUserId();
 		try {
-			workExperiences = gtsUserProjectRepository.findBygtsUserId(GTS_USER_ID);
+			projects = gtsUserProjectRepository.findBygtsUserId(GTS_USER_ID);
+			logger.info("projects are"+projects.toString());
 			
-			if(workExperiences.isEmpty()){
+			if(projects.isEmpty()){
+				logger.error("No Projects for the particular UserId");	
 			return (T) new WorkExperienceResponseBody("No Projects for the particular UserId","false",404);	
+			
 			}
 			else{
-				return (T) workExperiences;
+				return (T) projects;
 			}
 		} catch (Exception e) {
+			logger.error("No Projects for the particular UserId");
 			e.printStackTrace();
 			return (T) new WorkExperienceResponseBody("No Projects for the particular UserId ","false",404);	
 			
@@ -52,8 +59,10 @@ public class GtsUserProjectController<T> {
 		try {
 			
 			gtsUserProject = gtsUserProjectRepository.findById(projectId.getProjectId());
+			logger.info("Recieved the UserProject and it is"+gtsUserProject);
 		if(gtsUserProject.get()==null){
-			return (T) new WorkExperienceResponseBody("No Project for the particular WorkExperienceId","false",404);	
+			logger.error("No Project for the particular ProjectId");
+			return (T) new WorkExperienceResponseBody("No Project for the particular ProjectId","false",404);	
 			
 		}
 		else{
@@ -61,7 +70,7 @@ public class GtsUserProjectController<T> {
 		}
 		} catch (Exception e) {
 			e.printStackTrace();
-			return (T) new WorkExperienceResponseBody("No Project for the particular WorkExperienceId","false",404);
+			return (T) new WorkExperienceResponseBody("No Project for the particular ProjectId","false",404);
 		}
 		
 
@@ -73,12 +82,12 @@ public class GtsUserProjectController<T> {
 		try {
 			
 			gtsUserProjectRepository.save(gtsUserProjects);
-			
+			logger.info("Added new project ");
             return new WorkExperienceResponseBody("Added New Project","true",200);
 		
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+			logger.error("Exception while adding New Project ");
 			return new WorkExperienceResponseBody("Exception while adding New Project","false",500);
 		}
 	}
@@ -88,9 +97,11 @@ public class GtsUserProjectController<T> {
 		try {
 
 			gtsUserProjectRepository.save(gtsUserProjects);
+			logger.info("Updated the project ");
 			return new WorkExperienceResponseBody("updated the Project","true",200);
 		} catch (Exception e) {
 			e.printStackTrace();
+			logger.error("exception while updating the Project");
 			return new WorkExperienceResponseBody("exception while updating the Project","false",500);
 		}
 	}
@@ -100,14 +111,17 @@ public class GtsUserProjectController<T> {
 		try {
 			
 			if((gtsUserProjectRepository.findById(projectId.getProjectId()))==null){
-
+                logger.error("Project doesnot exist for the workExperienceId");
 				return new WorkExperienceResponseBody( "Project doesnot exist for the workExperienceId","false",404);}
 			else{
 				gtsUserProjectRepository.deleteById(projectId.getProjectId());
-				return new WorkExperienceResponseBody("Deleted the Project","true",200);		
+				logger.info("Deleted the project");
+				return new WorkExperienceResponseBody("Deleted the Project","true",200);
+				
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			logger.error("Exception while deleting the Project");
 			return new WorkExperienceResponseBody( "Exception while deleting the Project","false",500);
 		}
 	}
@@ -115,12 +129,15 @@ public class GtsUserProjectController<T> {
 	public WorkExperienceResponseBody deleteProjectsByUserId(@RequestBody UserId userId ) {
 	
 		Long GTS_USER_ID=userId.getUserId();
-			try {workExperiences=gtsUserProjectRepository.findBygtsUserId(GTS_USER_ID);
+			try {projects=gtsUserProjectRepository.findBygtsUserId(GTS_USER_ID);
 			
-			if(workExperiences.isEmpty()){
+			if(projects.isEmpty()){
+				logger.error("project doesn't exist for the userId");
 				return new WorkExperienceResponseBody("Project doesnot exist for the UserId", "false",404);}
+			
 			else{
 				gtsUserProjectRepository.deleteBygtsUserId(GTS_USER_ID);
+				logger.info("deleted the project");
 				return new WorkExperienceResponseBody("Deleted the Project","true",200);		
 			}
 				
